556ca601f80f7032639fe9eb5c5a7b54943c2994
Maik Emanoel <maikemanoel11@gmail.com>
2025-02-22 00:20:49 -0300

    third class

--- src/main/java/br/com/nlw/events/repo/SubscriptionRepo.java
+++ src/main/java/br/com/nlw/events/repo/SubscriptionRepo.java
@@ -1,11 +1,32 @@
 package br.com.nlw.events.repo;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import br.com.nlw.events.dto.SubscriptionRankingItem;
 import br.com.nlw.events.model.Event;
 import br.com.nlw.events.model.Subscription;
 import br.com.nlw.events.model.User;
 
 public interface SubscriptionRepo extends CrudRepository<Subscription, Integer> {
   public Subscription findByEventAndSubscriber(Event evt, User user);
+
+  @Query(value = "select\r\n" + //
+      "  count(indication_user_id) as quantidade,\r\n" + //
+      "  indication_user_id,\r\n" + //
+      "  user_name\r\n" + //
+      "from\r\n" + //
+      "  tbl_subscription tbls\r\n" + //
+      "  inner join tbl_user on tbls.indication_user_id = tbl_user.user_id\r\n" + //
+      "where\r\n" + //
+      "  indication_user_id is not null\r\n" + //
+      "  and event_id = 5\r\n" + //
+      "group by\r\n" + //
+      "  indication_user_id\r\n" + //
+      "order by\r\n" + //
+      "  quantidade DESC", nativeQuery = true)
+  public List<SubscriptionRankingItem> generateRanking(@Param("eventId") Integer eventId);
 }
--- src/main/java/br/com/nlw/events/service/SubscriptionService.java
+++ src/main/java/br/com/nlw/events/service/SubscriptionService.java
@@ -1,8 +1,13 @@
 package br.com.nlw.events.service;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.nlw.events.dto.SubscriptionRankingByUser;
+import br.com.nlw.events.dto.SubscriptionRankingItem;
 import br.com.nlw.events.dto.SubscriptionResponse;
 import br.com.nlw.events.exception.EventNotFoundException;
 import br.com.nlw.events.exception.SubscriptionConflictException;
@@ -39,10 +44,14 @@ public class SubscriptionService {
       userRec = userRepo.save(user);
     }
 
-    User indicador = userRepo.findById(userId).orElse(null);
+    User indicador = null;
 
-    if (indicador == null) {
-      throw new UserIndicadorNotFoundException("Usuário "+userId+ " indicador não existe");
+    if (userId != null) {
+      indicador = userRepo.findById(userId).orElse(null);
+
+      if (indicador == null) {
+        throw new UserIndicadorNotFoundException("Usuário " + userId + " indicador não existe");
+      }
     }
 
     Subscription subs = new Subscription();
@@ -60,5 +69,31 @@ public class SubscriptionService {
     Subscription res = subRepo.save(subs);
     return new SubscriptionResponse(res.getSubscriptionNumber(),
         "http://codecraft.com/subscription/" + res.getEvent().getPrettyName() + "/" + res.getSubscriber().getId());
+  }
+
+  public List<SubscriptionRankingItem> getCompleteRanking(String prettyName) {
+    Event evt = evtRepo.findByPrettyName(prettyName);
+
+    if (evt == null) {
+      throw new EventNotFoundException("Ranking do evento " + prettyName + " não existe");
+    }
+
+    return subRepo.generateRanking(evt.getEventId());
+  }
+
+  public SubscriptionRankingByUser getRankingByUser(String prettyName, Integer userId) {
+    List<SubscriptionRankingItem> ranking = getCompleteRanking(prettyName);
+
+    SubscriptionRankingItem item = ranking.stream().filter(i -> i.userId().equals(userId)).findFirst().orElse(null);
+
+    if (item == null) {
+      throw new UserIndicadorNotFoundException(
+          "Não há inscrições com indicação do usuário " + userId);
+    }
+
+    Integer posicao = IntStream.range(0, ranking.size()).filter(pos -> ranking.get(pos).userId().equals(userId))
+        .findFirst().getAsInt();
+
+    return new SubscriptionRankingByUser(item, posicao + 1);
   }
 }
